130965d307c762a99e3c06c6870c1c88bf20232f
Yegenoglu, Alper <a.yegenoglu@fz-juelich.de>
2024-07-23 14:20:42 +0200

    increased fontsize and changed to eps format

--- deviation_plots.py
+++ deviation_plots.py
@@ -23,24 +23,27 @@ types.append(types.pop(types.index('Satisfaction')))
 save_name = ""
 if file_name in ['SGA1-2_summary2.xlsx', 'YRE Surveys_SGA1-2_summary.xlsx']:
     categories = ['Failed', 'Insufficient', 'Sufficient', 'Satisfactory', 'Good', 'Excellent']
-    save_name = f"conf_ratings_sga1-sga2_deviation.pdf" if file_name == 'SGA1-2_summary2.xlsx' else f'YRE_deviations_sga1-2.pdf'
+    save_name = f"conf_ratings_sga1-sga2_deviation.eps" if file_name == 'SGA1-2_summary2.xlsx' else f'YRE_deviations_sga1-2.eps'
 else:
     categories = ["Very poor", "Poor", "Fair", "Good", "Excellent"]
-    save_name = f"conf_deviations_sga3.pdf" if file_name =='SGA3_summary.xlsx' else f'YRE_deviations_sga3.pdf'
+    save_name = f"conf_deviations_sga3.eps" if file_name =='SGA3_summary.xlsx' else f'YRE_deviations_sga3.eps'
 
-# Calculate mean ratings for each type-class combination
+# Sum up responses for each class
 ratings_dict = {}
 for c in classes:
     df_class = df[df['Class'] == c]
     if not df_class.empty:
-        std = df_class.sum(numeric_only=True)
-        ratings_dict[c] = std
+        ratings_dict[c] = df_class.sum(numeric_only=True)
 
 # Convert dictionary to DataFrame
 ratings_df = pd.DataFrame(ratings_dict)
 # sort by class name
 ratings_df.sort_index(axis=1, inplace=True)
-sns.catplot(kind='box', data=ratings_df, errorbar="sd",  height=6, aspect=1.3, orient='h', palette='Reds')
+# Set the font size globally
+plt.rcParams['xtick.labelsize'] = 13
+plt.rcParams['ytick.labelsize'] = 13
+g = sns.catplot(kind='box', data=ratings_df, errorbar="sd",  height=6, aspect=1.3, orient='h', palette='Reds')
+g.set_xlabels('Response', size=13)
 # Save figure
-# plt.savefig(save_name, dpi=600, bbox_inches='tight', pad_inches=0.1)
-plt.show()
+plt.savefig(save_name, dpi=600, bbox_inches='tight', pad_inches=0.1)
+# plt.show()
